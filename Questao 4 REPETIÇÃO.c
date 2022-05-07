3c104b9fada832b02fbc8fa1bf213fa412ff32fb
Taiga <102325560+Taiga-Naka@users.noreply.github.com>
2022-05-07 14:26:14 -0300

    Create Questao 4 REPETIÇÃO.c

---
+++ Questao 4 REPETIÇÃO.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) {
+	//Questao 4 perguntar pro sensei
+	int qntNotas, contadorNotas; 
+	float nota1, nota2, media;
+	
+	printf("Digite a Quantidade de notas da sua turma: ");
+	scanf("%i", &qntNotas);
+	fflush(stdin);
+	
+	for(contadorNotas = qntNotas; contadorNotas <= qntNotas; contadorNotas++) {
+			
+			printf("\nDigite a Primeira Nota: ");
+			scanf("%f", &nota1);
+			fflush(stdin);
+			printf("\nDigite a Segunda Nota: ");
+			scanf("%f", &nota2);
+			fflush(stdin);
+	}	
+			media = (nota1 + nota2) / 2;
+	
+	 	printf("\n--------------------------------\n");
+		printf("\nA media Total da turma eh: %.1f\n", media / qntNotas);
+		
+	return 0;
+}
