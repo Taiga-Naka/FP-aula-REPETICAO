72bace5dd4471e23f48923122c8e07c5600066d1
Taiga <102325560+Taiga-Naka@users.noreply.github.com>
2022-05-07 15:58:32 -0300

    Update Questao 5 REPETIÇÃO.c
    DEKETA

--- Questao 5 REPETIÇÃO.c
+++ Questao 5 REPETIÇÃO.c
@@ -3,26 +3,29 @@
 
 int main (void) {
 	//Questao 5
-	int numAlunos, contador, qntTurma, media;
+	int numAlunos, contador, qntTurma, somaNumAlunos;
 	
 	printf("Digite a Quantidade da Turma: ");
 	scanf("%i", &qntTurma);
 	fflush(stdin);
 	
-	for(contador = qntTurma; contador >= qntTurma; contador++) {
+	
+	for(contador = 0; contador < qntTurma; contador++) {
 		
-		printf("\nDigite Quantos Alunos tem em cada Turma: ");
+		printf("\nDigite Quantos Alunos tem em cada Turma? \nTurma %i: ", contador + 1);
 		scanf("%i", &numAlunos);
 		fflush(stdin);
 		
-		if(numAlunos <= 40) {
-			printf("A Media da Turma eh: %i", numAlunos / contador);
-			break;
-		} else {
-			printf("DIGITE ATE 40 ALUNOS!!");
-		}
-	}
-
+		somaNumAlunos += numAlunos;
+		
+	}	
+	
+	if(numAlunos <= 40 && numAlunos > 0) {	
+		printf("\nA Media das Turmas eh: %i", somaNumAlunos / qntTurma);
+	} else {
+		printf("\nDIGITE ATE 40 ALUNOS!!\n E NAO DIGITA QUALQUER COISA!!");
+	} 
+	
 	return 0;
 } 
 	
