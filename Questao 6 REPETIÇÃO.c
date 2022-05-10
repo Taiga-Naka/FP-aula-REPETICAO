86f1e92cea6f9f9eff71782a571dca7df314e0e1
Taiga <102325560+Taiga-Naka@users.noreply.github.com>
2022-05-09 21:40:32 -0300

    Update Questao 6 REPETIÇÃO.c
    DEKITA

--- Questao 6 REPETIÇÃO.c
+++ Questao 6 REPETIÇÃO.c
@@ -2,25 +2,30 @@
 #include <stdlib.h>
 
 int main (void) {
-	//Questao 6
-
-	int cdQnt, cadaCD, contador;
-	float somaCD;
+	//Questao 5
+	int numAlunos, contador, qntTurma, somaNumAlunos;
 	
-	printf("Informe a Quantidade de CDs: ");
-	scanf("%i", &cdQnt);
+	printf("Digite a Quantidade da Turma: ");
+	scanf("%i", &qntTurma);
 	fflush(stdin);
 	
-	for(contador = 0; contador < cdQnt; contador++) {
-		printf("\nDigite o Valor do %i CD: ", contador + 1);
-		scanf("%i", &cadaCD);
+	
+	for(contador = 0; contador < qntTurma; contador++) {
+		
+		printf("\nDigite Quantos Alunos tem em cada Turma? \nTurma %i: ", contador + 1);
+		scanf("%i", &numAlunos);
 		fflush(stdin);
 		
-		somaCD += cadaCD;
-	}
+		if(numAlunos >= 40 && numAlunos < 0) {
+			somaNumAlunos += numAlunos;
+			printf("\nA Media das Turmas eh: %i", somaNumAlunos / qntTurma);
+		} else {
+			printf("\nDIGITE ATE 40 ALUNOS!!\n E NAO DIGITA QUALQUER COISA!!");
+			contador--;
+		}
+	}	
 	
-	printf("\nO Valor Total de CD eh: R$%.2f", somaCD);
-	printf("\nO Media total do custo do CD eh: R$%.2f", somaCD / cdQnt);
-
+	
 	return 0;
 } 
+	
