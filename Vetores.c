a63c3c43daa34e9ec10881515a2a457bd05db832
Taiga <102325560+Taiga-Naka@users.noreply.github.com>
2022-06-06 19:56:01 -0300

    Update Vetores.c

--- Vetores.c
+++ Vetores.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define tamanho 10
 
 int main (void) {
+	//Questao 1 MATRIZES E VETORES
+	int num [9], cont;
 	
+	for (cont = 0; cont < tamanho; cont++) {
+		printf("Digite os numeros: ");
+		scanf("%i", &num [cont]);
+		fflush(stdin);
+	}
 	
-	
+	printf("Resultado: %i", num [0] + num [1] + num [2] + num [3] + num [4] + num [5] + num [6] + num [7] + num [8] + num [9]);
 	
 	return 0;
 }
