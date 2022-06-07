cdee27fbfe57cf1e8f5083e0e0b7cb8eb9145345
Taiga <102325560+Taiga-Naka@users.noreply.github.com>
2022-06-06 22:19:27 -0300

    Update Q5 Vetores.c

--- Q5 Vetores.c
+++ Q5 Vetores.c
@@ -2,7 +2,19 @@
 #include <stdlib.h>
 
 int main (void) {
-	/* 5. Construa um algoritmo que leia um vetor de 15palavrase mostre-os na ordem inversa.*/
+	/* 5. Construa um algoritmo que leia um vetor de 15 palavras e mostre-os na ordem inversa.*/
+	int cont;
+	char texto[50][15];//matiz de caractere
+	
+	for(cont = 0; cont < 15; cont++) {
+		printf("Digite %i palavra: ", cont + 1);
+		gets(texto[cont]);
+		fflush(stdin);
+	}
+	
+	for(cont = 15; cont > 0; cont--) {
+		printf("\n%s", texto[cont]);
+	}
 	
 	return 0;
 }
