00f826c204a23d8ae7c1abad1c1cfd5cb1f1fc03
Taiga <102325560+Taiga-Naka@users.noreply.github.com>
2022-06-13 21:27:05 -0300

    aa

---
+++ Q 8 Matrizes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) {
+	//Questao 9 MATRIZ
+	/*Construa  um  algoritmo  que  leia  uma  matriz  quadrada  de  ordem  4.  
+	Leia  uma constante K, multiplique a diagonal principal por esta constante e imprima a matriz multiplicada.*/
+	 int matriz[4][4], result[4][4], i, j, k;
+	
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			printf("Digite matriz [%i][%i]: ", i, j);
+			scanf("%i", &matriz[i][j]);
+			fflush(stdin);
+		}	
+	}
+	
+	printf("Digite o valor do K: ");
+	scanf("%i", &k);
+	fflush(stdin);
+	
+	for (i = 0; i < 4; i++) {
+		result[i][i] = matriz[i][i] * k;
+	}
+	
+	printf("\nresultado:\n\n");
+	
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			printf("%i\t", result[i][j]);
+		}	
+		printf("\n");
+	}
+	
+	return 0;
+}
